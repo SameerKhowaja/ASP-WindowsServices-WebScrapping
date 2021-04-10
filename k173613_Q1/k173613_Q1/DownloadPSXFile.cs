971f16f71f8c914113adfce5ff5d6e4ea41da034
Sameer Khowaja - SK <54762751+SameerKhowaja@users.noreply.github.com>
2021-04-10 18:13:48 +0500

    Q1 Done

---
+++ k173613_Q1/k173613_Q1/DownloadPSXFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Net.Http;
+using System.IO;
+
+namespace k173613_Q1
+{
+    class DownloadPSXFile
+    {
+        private readonly Timer _timer;
+        string file_path = ConfigurationManager.AppSettings.Get("FilePath");        // Where to Save when Download
+        string file_url = ConfigurationManager.AppSettings.Get("PSX-URL-Path");     // Link from where to Download
+
+        public DownloadPSXFile()
+        {
+            _timer = new Timer(1000 * 60 * 5) { AutoReset = true };     // Every Five Minute
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            String url = file_url;
+            String path = file_path;
+
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+
+            int day = DateTime.Today.Day;
+            int month = DateTime.Today.Month;
+            int year = DateTime.Today.Year;
+
+            DateTime date = new DateTime(year, month, day);
+            //string file_name = @path + "/Summary" + date.ToString("dd") + date.ToString("MMM") + date.ToString("yy") + ".html";
+            string file_name = @path + "/Summary-PSX-k173613-Q1.html";
+
+            string content = html.Result;
+            File.WriteAllText(file_name, content);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
---
+++ k173613_Q1/k173613_Q1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace k173613_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<DownloadPSXFile>(s =>
+                {
+                    s.ConstructUsing(downloadPSXFile => new DownloadPSXFile());
+                    s.WhenStarted(downloadPSXFile => downloadPSXFile.Start());
+                    s.WhenStopped(downloadPSXFile => downloadPSXFile.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("DownloadPSX");
+                x.SetDisplayName("Download PSX File By K173613_Q1");
+                x.SetDescription("This service will download PSX data as save as html file every 5 minutes. Developed By Sameer Khowaja K173613_Q1.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
