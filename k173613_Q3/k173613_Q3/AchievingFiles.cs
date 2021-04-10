eea1c0e72d5f9b55c0c5db7a39d9f5723036cb33
Sameer Khowaja - SK <54762751+SameerKhowaja@users.noreply.github.com>
2021-04-10 18:14:30 +0500

    Q3 Done

---
+++ k173613_Q3/k173613_Q3/AchievingFiles.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace k173613_Q3
+{
+    class AchievingFiles
+    {
+        private readonly Timer _timer;
+
+        public AchievingFiles()
+        {
+            _timer = new Timer(1000 * 60 * 15) { AutoReset = true };     // Every 15 Minutes
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string Old_Dir_Path = ConfigurationManager.AppSettings.Get("FreshRecordsPath");   // Old Dir Path from where to get folders
+            string New_Dir_Path = ConfigurationManager.AppSettings.Get("PastRecordsPath");   // New Dir Path you want to save old folders
+
+            // Generate Folders
+            System.IO.Directory.CreateDirectory(New_Dir_Path);
+
+            // Get Lastly Created/Modified Folder Path
+            var directory = new DirectoryInfo(Old_Dir_Path);
+            var myFolder = (from f in directory.GetDirectories()
+                            orderby f.LastWriteTime descending
+                            select f).First();
+            String myFolder_str = Convert.ToString(myFolder);   // Latest Folder Name
+            // Console.WriteLine(myFolder);
+
+            // Get All folders names
+            var Folders = new DirectoryInfo(Old_Dir_Path).GetDirectories().Select(x => x.Name).ToArray();
+            // Console.WriteLine(Folders.Length);
+
+            if(Folders.Length > 1)
+            {
+                foreach (String folder_name in Folders)
+                {
+                    if (folder_name != myFolder_str)     // Except Recent Folder
+                    {
+                        try
+                        {
+                            Directory.Move(Old_Dir_Path + folder_name, New_Dir_Path + folder_name);
+                        }
+                        catch (IOException exp)
+                        {
+                            Console.WriteLine(exp.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ k173613_Q3/k173613_Q3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace k173613_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<AchievingFiles>(s =>
+                {
+                    s.ConstructUsing(achievingFiles => new AchievingFiles());
+                    s.WhenStarted(achievingFiles => achievingFiles.Start());
+                    s.WhenStopped(achievingFiles => achievingFiles.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("AchievingPSXData");
+                x.SetDisplayName("Achieving PSX Files Data By K173613_Q3");
+                x.SetDescription("This service is responsible to move all the old XML files except for the most recent one and store to another location every 15 minutes. Developed By Sameer Khowaja K173613_Q3.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
